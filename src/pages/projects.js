932549320b54bb98c1b6461ead3a236dea4b5f06
filipezz <filipezmarron@hotmail.com>
2019-12-26 03:57:56 -0300

    Adjust footer

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -13,7 +13,7 @@ function Projects({ data }) {
       <Appbar />
       <Helmet>
         <meta charSet="utf-8" />
-        <title>Projects | Filipe</title>
+        <title>Projects | Filipe Marron</title>
       </Helmet>
       <div className="row">
         {projectList.map(({ node }) => {
